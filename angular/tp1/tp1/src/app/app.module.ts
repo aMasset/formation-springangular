6d5d686045eacdca8195055c9d6d437b1acdb722
Axel Masset <axel.masset@uclouvain.be>
2021-02-10 14:29:09 +0100

    product details

--- angular/tp1/tp1/src/app/app.module.ts
+++ angular/tp1/tp1/src/app/app.module.ts
@@ -2,19 +2,26 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {ProductListComponent} from './product-list/product-list.component';
+import {ProductListComponent} from './product/product-list/product-list.component';
 import {FormsModule} from '@angular/forms';
 import {FakeProductService} from './service/fake-product.service';
 import {ProductService} from './service/product-service';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {NavigationBarComponent} from './navigation-bar/navigation-bar.component';
 import {RouterModule, Routes} from '@angular/router';
-import { FournisseurListComponent } from './fournisseur-list/fournisseur-list.component';
+import {FournisseurListComponent} from './fournisseur/fournisseur-list/fournisseur-list.component';
+import {ProductPageComponent} from './product/product-page/product-page.component';
+import {FournisseurPageComponent} from './fournisseur/fournisseur-page/fournisseur-page.component';
+import {ProductDetailComponent} from './product/product-detail/product-detail.component';
 
+const productsRoutes: Routes = [
+  {path: 'list', component: ProductListComponent},
+  {path: ':id', component: ProductDetailComponent},
+];
 
 const routes: Routes = [
   {path: '', redirectTo: '/products', pathMatch: 'full'},
-  {path: 'products', component: ProductListComponent},
+  {path: 'products', component: ProductPageComponent, children: productsRoutes},
   {path: 'fournisseurs', component: FournisseurListComponent},
 ];
 
@@ -23,7 +30,10 @@ const routes: Routes = [
     AppComponent,
     ProductListComponent,
     NavigationBarComponent,
-    FournisseurListComponent
+    FournisseurListComponent,
+    ProductPageComponent,
+    FournisseurPageComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
