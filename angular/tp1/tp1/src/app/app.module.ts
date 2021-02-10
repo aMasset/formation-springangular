35f24659e5a0cccbcee765163295ff43ac3b1ced
Axel Masset <axel.masset@uclouvain.be>
2021-02-10 12:14:51 +0100

    navigation fournisseurs

--- angular/tp1/tp1/src/app/app.module.ts
+++ angular/tp1/tp1/src/app/app.module.ts
@@ -6,19 +6,31 @@ import {ProductListComponent} from './product-list/product-list.component';
 import {FormsModule} from '@angular/forms';
 import {FakeProductService} from './service/fake-product.service';
 import {ProductService} from './service/product-service';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {NavigationBarComponent} from './navigation-bar/navigation-bar.component';
+import {RouterModule, Routes} from '@angular/router';
+import { FournisseurListComponent } from './fournisseur-list/fournisseur-list.component';
+
+
+const routes: Routes = [
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: 'products', component: ProductListComponent},
+  {path: 'fournisseurs', component: FournisseurListComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    NavigationBarComponent
+    NavigationBarComponent,
+    FournisseurListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    NgbModule
+    NgbModule,
+    RouterModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [{provide: ProductService, useClass: FakeProductService}],
   bootstrap: [AppComponent]
